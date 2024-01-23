cfa0e570b9e5f4d0e73ab1d793eb4274d832763a
Билгүүдэй Бүдшир <HP231031@G303-CC0131.local>
2024-01-23 17:31:31 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const cors = require('cors')
+
+
+const app = express()
+const port = 8080
+app.use(express.json())
+app.use(cors())
+
+app.get('/', (req,res) => {
+    res.send('hello world')
+})
+
+app.listen(port, () => {
+    console.log(`you backend server is running on ${port}`)
+})
