b1f2228e3f9703d969767d36184820fdc5dd548c
Билгүүдэй Бүдшир <HP231031@G303-CC0131.local>
2024-04-11 19:22:04 +0800

    Controller

--- Controller/transactionController.js
+++ Controller/transactionController.js
@@ -5,7 +5,7 @@ const createTransaction = async (req, res) => {
   try {
     const response = await transactionModel.create(data);
     console.log(response);
-    res.status(200).send(response);
+    res.status(200).send(`${response._id} Амжилттай боллоо`);
   } catch (error) {
     res.status(500).send(error);
   }
@@ -19,5 +19,27 @@ const getTransaction = async (req, res) => {
     res.status(500).send(error);
   }
 };
+const deleteTransaction = async (req, res) => {
+  const transactionId = req.params.transactionId;
+  try {
+    const response = await transactionModel.findByIdAndDelete(transactionId);
+    res.status(200).send(`${response._id} Амжилттай устлаа`);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+};
+const updateTransaction = async (req, res) => {
+  const transactionId = req.params.transactionId;
+  const body = req.body;
+  try {
+    const response = await transactionModel.findByIdAndUpdate(
+      transactionId,
+      body
+    );
+    res.status(200).send(response);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+};
 
-module.exports = { createTransaction, getTransaction };
+module.exports = { createTransaction, getTransaction, deleteTransaction, updateTransaction };
--- routes/transactionRoutes.js
+++ routes/transactionRoutes.js
@@ -2,11 +2,16 @@ const { Router } = require("express");
 const {
   getTransaction,
   createTransaction,
+  deleteTransaction,
+  updateTransaction,
 } = require("../Controller/transactionController");
 
 const transactionRoute = Router();
 
 transactionRoute.post("/create-transaction", createTransaction);
 transactionRoute.get("/get-transaction", getTransaction);
+transactionRoute.delete("/delete-transaction/:transactionId", deleteTransaction)
+transactionRoute.post("/update-transaction/:transactionId", updateTransaction)
+
 
 module.exports = transactionRoute;
